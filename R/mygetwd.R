b4d0a3e148a370c35dab7deb8f36527c36de7ac5
Cole Hartman <Col3@ou.edu>
2022-01-19 11:16:53 -0600

    initial start

---
+++ R/mygetwd.R
@@ -0,0 +1,12 @@
+#' @title This finds my directory
+#'
+#' @description This is where we put more stuff
+#'
+#' @return This function returns the working directory
+#' @export
+#'
+#' @examples
+#' mygetwd()
+mygetwd <- function(){
+  getwd()
+}
