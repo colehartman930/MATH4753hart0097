05705b7f6d063b94c0084e85a24e879608822c9a
Cole Hartman <Col3@ou.edu>
2022-03-10 14:42:59 -0600

    DDT Function

---
+++ R/myddt.R
@@ -0,0 +1,33 @@
+#' Title DDT Curve Maker
+#'
+#' @param df The Data Frame you are using
+#' @param species What species of fish you are interested in making data of
+#'
+#' @return A plot of weight vs length for a specific species, as well as this information being output in a csv file. In the command line you can also see the data used
+#' @export
+#'
+#' @examples
+#' \dontrun{myddt(ddt, species = "CCATFISH")}
+
+myddt = function(df, species){
+
+  ddt2 <- ddt[ddt$SPECIES == species,]  #Subsets our data frame so we can work with the correct species.
+
+  fishcol=with(ddt2,ifelse(RIVER=="FCM","Red", ifelse(RIVER=="LCM","Blue", ifelse(RIVER=="TRM", "Purple", "Green"))))  # Colors the river something unique for each instance.
+
+  write.csv(ddt2, paste0("LvsWfor", species, ".csv")) # Creates our csv output file
+
+  g <- ggplot(ddt2, aes_string(x = ddt2$LENGTH, y = ddt2$WEIGHT)) # Creates our plot and gives values to the x and y axis
+
+  g = g + geom_point(x = ddt2$LENGTH, y = ddt2$WEIGHT) # Adds points
+
+  g = g + geom_point(aes(fill = RIVER), color = fishcol) # Colors our points in accordance to which river they were found in and makes our legend
+
+  g = g + geom_smooth(formula = y~x +I(x^2), method = "lm") # Adds a curve to the plot
+
+  g = g + labs(title = paste0("COLE HARTMAN PLOT FOR ", species), x = "LENGTH", y = "WEIGHT") #Adds labels to our plot
+  print(g) #Prints the plot
+  print(ddt) #Prints the data frame to the command line
+  print(ddt2) #Prints the subsetted data frame to the command line
+  table(ddt$RIVER) / length(ddt$RIVER) #Gives a relative frequency table for the river data.
+}
